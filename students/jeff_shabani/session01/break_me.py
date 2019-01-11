2514622b6f726e251ab50f64af8412f0ffc124da
Rockwell Shabani <23247076+Rockwell70@users.noreply.github.com>
2019-01-11 13:52:19 -0800

    Create break_me.py

---
+++ students/jeff_shabani/session01/break_me.py
@@ -0,0 +1,15 @@
+def error_name(x):
+    return z
+
+
+def error_type(a):
+    a = str(a)
+    return a/2
+
+
+def error_syntax(k):
+    rerun isinstance(k, list)
+
+import datetime
+def error_attribute():
+    return datetime.date.quarter('01.01.12')
