c383b9fefc79313adaf9f79d174f904d448ce517
Douglas Klos <dougklos@gmail.com>
2019-01-17 19:19:40 -0800

    fizzbuzz.py working

---
+++ students/douglas_klos/session2/fizzbuzz.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+# Douglas Klos
+# January 17th, 2019
+# Python 210, Assignment 2
+# fizzbuzz.py
+
+# For loop is not inclusive, so we run it to 101 to include 100.
+for num in range(1,101):
+    # If the number is divisible by 3, then we print Fizz
+    # We don't want a newline yet, as if it also divisible by 5 we'll add to the line.
+    if num % 3 == 0:
+        print('Fizz',end='')
+    # If the number is divisible by 5, then we print Buzz
+    # While we want a newline always after buzz, it is added after the conditionals.
+    if num % 5 == 0:
+        print('Buzz',end='')
+    # If the number is neither divisible by 3 nor 5, we print the number itself.
+    # Still no newline at this point.
+    if (num % 3 != 0 and num % 5 != 0):
+        print(num,end='')
+    # Here's our newline.  This allows the first two if's to work together in displaying FizzBuzz.
+    print()
+
