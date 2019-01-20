f507bd424999bc6680ef990d3d0e3d0669c66f2b
Douglas Klos <dougklos@gmail.com>
2019-01-20 09:45:58 -0800

    pep8 fixed on grid_printer.py

--- students/douglas_klos/session2/grid_printer.py
+++ students/douglas_klos/session2/grid_printer.py
@@ -5,6 +5,7 @@
 # Python 210, Assignment 2
 # grid_printer.py
 
+
 def print_grid0():
     """
     Simply prints the static 2 x 2 grid shown at the start of the assignment.
@@ -14,12 +15,14 @@ def print_grid0():
 
     print(str1 + str2 * 4 + str1 + str2 * 4 + str1)
 
+
 def print_grid1(x):
     """
-    Prints a 2 x 2 grid.
-    Value passed to function is the total width and height of the grid sans edges.
-    Since grid has static two rows and two columns, odd numbers are floored during division.
-    Problem was underspecified, this seemed reasonable, output matches examples given.
+    Prints a 2 x 2 grid.  Value passed to function is the total width
+    and height of the grid sans edges.  Since grid has static two rows
+    and two columns, odd numbers are floored during division.  Problem
+    was underspecified, this seemed reasonable, output matches examples
+    given.
     """
     if x < 0:
         print('Enter a number 0 or larger for cell size')
@@ -30,7 +33,8 @@ def print_grid1(x):
     str2 = ('| ' + '  ' * size + '| ' + '  ' * size + '|' + '\n')
     print(str1 + str2 * size + str1 + str2 * size + str1)
 
-def print_grid2(x,y):
+
+def print_grid2(x, y):
     """
     Prints a grid of specified size.
     X is the number of rows & columns.
@@ -45,15 +49,12 @@ def print_grid2(x,y):
             print((('| ' + '  ' * y) * x) + '|')
     print((('+ ' + '- ' * y) * x) + '+\n')
 
-if __name__ == "__main__":
-    """
-    grid_printer.main
 
-    """
+if __name__ == "__main__":
+    """ grid_printer.main """
     print_grid0()
     print_grid1(3)
     print_grid1(11)
     print_grid1(15)
-    print_grid2(3,4)
-    print_grid2(5,3)
-
+    print_grid2(3, 4)
+    print_grid2(5, 3)
