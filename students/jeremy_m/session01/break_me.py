d7f3fbf8371908a6a1749f1b2655278bf3ee7f98
Jeremy <jereamon@gmail.com>
2019-01-21 14:52:00 -0800

    Added my break_me.py solution

---
+++ students/jeremy_m/session01/break_me.py
@@ -0,0 +1,35 @@
+# def namey():
+#     return biscuit
+
+# namey()
+
+
+
+
+# def typey():
+#     a_list = ['one', 'two', 'three']
+#     return a_list + 4
+
+# typey()
+
+
+
+
+# def syntaxy():
+#     print 'hello'
+#     return
+
+# syntaxy()
+
+
+
+
+
+class attributer():
+    def __init__(self):
+        return "placeholder"
+
+def callAttributer():
+    return attributer.test()
+
+print(callAttributer())
