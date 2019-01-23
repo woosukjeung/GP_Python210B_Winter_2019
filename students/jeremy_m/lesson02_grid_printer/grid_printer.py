fbf17a835e828aec5ccd99db25ef8cf4acfb2d66
Jeremy <jereamon@gmail.com>
2019-01-23 10:27:31 -0800

    Grid Printer 2nd commit. Now it's in a function and you can pass an argument that will set the grids length and width.

--- students/jeremy_m/lesson02_grid_printer/grid_printer.py
+++ students/jeremy_m/lesson02_grid_printer/grid_printer.py
@@ -1,16 +1,15 @@
 p = '+'
-m = '-'
-s = ' '
 l = '|'
 
-print(p + (m * 4) + p + (m * 4) + p)
+def grid_printer(g_size):
+    half_g = g_size // 2
+    m = '-' * (half_g)
+    s = ' ' * (half_g)
 
-for i in range(4):
-    print(l + (s * 4) + l + (s * 4) + l)
+    for i in range(2):
+        print(p + m + p + m + p)
+        for i in range(half_g):
+            print(l + s + l + s + l)
+    print(p + m + p + m + p)
 
-print(p + (m * 4) + p + (m * 4) + p)
-
-for i in range(4):
-    print(l + (s * 4) + l + (s * 4) + l)
-
-print(p + (m * 4) + p + (m * 4) + p)
+grid_printer(15)
