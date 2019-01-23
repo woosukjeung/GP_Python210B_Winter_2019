1024e72947527c4630be816f91be1f3f6220bb45
Rockwell70 <rockwellshabani@gmail.com>
2019-01-22 17:41:29 -0800

    Shabani repo

---
+++ students/jeff_shabani/session02/series.py
@@ -0,0 +1,26 @@
+def fibonacci(n):
+    count = 0
+    a,b = 0,1
+    while count < n:
+        #begin fibonacci sequence: a now = 1, b now = 0+1
+        a, b = b, a+b
+        #increment count by 1 with each iteration
+        count +=1
+    if count == n:
+        #return result when nth fibonnci is calculated.
+        return a
+
+def lucas(n):
+    count = 0
+    a,b = 2,1
+    while count < n:
+        #begin fibonacci sequence: a now = 1, b now = 0+1
+        a, b = b, a+b
+        #increment count by 1 with each iteration
+        count +=1
+    if count == n:
+        #return result when nth fibonnci is calculated.
+        return a
+
+
+
