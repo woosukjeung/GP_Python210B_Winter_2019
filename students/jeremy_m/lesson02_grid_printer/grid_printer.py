6ec9a9345ff3f63a3fe8f42e044061bc7033c843
Jeremy <jereamon@gmail.com>
2019-01-23 10:14:07 -0800

    First grid printer commit. It will print a grid but it's not in a function or malleable.

---
+++ students/jeremy_m/lesson02_grid_printer/grid_printer.py
@@ -0,0 +1,16 @@
+p = '+'
+m = '-'
+s = ' '
+l = '|'
+
+print(p + (m * 4) + p + (m * 4) + p)
+
+for i in range(4):
+    print(l + (s * 4) + l + (s * 4) + l)
+
+print(p + (m * 4) + p + (m * 4) + p)
+
+for i in range(4):
+    print(l + (s * 4) + l + (s * 4) + l)
+
+print(p + (m * 4) + p + (m * 4) + p)
