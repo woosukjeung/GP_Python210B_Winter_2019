548651e9a0779e3a9808c87fd0d6d813c60c1b8b
woosukjeung <47073569+woosukjeung@users.noreply.github.com>
2019-02-11 19:19:52 -0800

    Update TrigramWJ.py
    i tried something else to make the code more compact and tried to stick more to the example given on the site but can't seem to fix the problem where the output is super short

--- students/WooseokJ/TrigramWJ.py
+++ students/WooseokJ/TrigramWJ.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[3]:
-
-
 import sys 
 import random
 
@@ -15,36 +9,29 @@ def words_from_file(filename):
 #from the list makes a dictiondary to mix it in the next process
 def build_trigrams_dict(words):
     trigrams = {}
-    for i, w in enumerate(words): 
-        if i + 2 < len(words): 
-            first = w.strip() 
-            second = words[i + 1].strip() 
-            third = words[i + 2].strip() 
-            mix = '{} {}'.format(first, second) 
-            if mix not in trigrams.keys(): 
-                trigrams[mix] = [] 
-            trigrams[mix].append(third) 
-    return trigrams 
-#this mixes the words to create the trigram 
-def build_text(trigrams, num, new_words=[]):
-    if len(new_words) >= int(num) or int(num) < 3: 
-        return ' '.join(new_words) 
-    if not new_words: 
-        second_start = random.choice(list(trigrams.keys())) 
-        new_words.extend(second_start.split(' ')) 
-    if len(new_words) > 1: 
-        mix = '{} {}'.format(new_words[-2], new_words[-1])
-        if mix not in trigrams.keys(): 
-            mix = random.choice(list(trigrams.keys()))
-        third = random.choice(trigrams[mix]) 
-        new_words.append(third) 
+    for i in range(len(words)-2):
+        pair = tuple(words[i:i + 2])
+        follower = words[i + 2]
+        if pair not in trigrams:
+            trigrams[pair] = [follower]
+        else:
+            trigrams[pair] += [follower]
+    return trigrams
+#this mixes the words to create the new text
+def build_text(pairs):
+    random_text = []
+    while len(random_text) < len(pairs):
+        random_key = random.choice(list(pairs.keys()))
+        value = pairs.get(random_key)
+        random_text.append(list(random_key))
+        random_text.append(value)
+        new_list = [item for fixedlist in random_text for item in fixedlist]
+        clean_text = " ".join(new_list)
 #this is a promot that asks for the file that we want to scramble make sure to add .txt at the end of the file
-        return build_text(trigrams, num, new_words)
+        return clean_text
 if __name__ == '__main__':
-    filename = input('Please enter a filename:\n--->')
-    length = 178
+    filename = input('Please enter a filename:')
     words = words_from_file(filename)
     trigrams = build_trigrams_dict(words)
-    new_text = build_text(trigrams,length)
+    new_text = build_text(trigrams)
     print (new_text) 
-
